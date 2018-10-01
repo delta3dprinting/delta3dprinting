ac9848415baba2bc0cc91ee6332ceb5f9b3f2734
Carl Velasco <carlvelasco96@gmail.com>
2018-10-01 14:40:58 +1300

    test
    Corrected form

--- components/global/Navigation/Profile/Visitor/Login/login.js
+++ components/global/Navigation/Profile/Visitor/Login/login.js
@@ -12,7 +12,7 @@ const loginObject = new ProfileDrawerObject(loginId, loginName, loginMethod);
 // Create the Login Form HTML
 const loginModalHTML =
   "<div id='modal_login_form_body'>" +
-  "<form='login_form' action='/users/login' method='POST' onsubmit=''>" +
+  "<form id='login_form' action='/users/login' method='POST' onsubmit=''>" +
   "<div id='username_field_body' class='input_field_body'>" +
   "<div id='username_field_title' class='input_field_title'>Username</div>" +
   "<input type='text' name='username' id='username_field' class='input_field'>" +
