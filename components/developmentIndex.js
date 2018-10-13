00c40ec1226c2f5158fcd472aef684fec20bc943
Carl Velasco <carlvelasco96@gmail.com>
2018-10-13 15:58:35 +1300

    test

--- components/developmentIndex.js
+++ components/developmentIndex.js
@@ -58,3 +58,76 @@ const isUserLoggedIn = (pointer, callback) => {
     callback();
   });
 };
+
+/* ==================================== SUBMIT NEW ORDER ==================================== */
+/* ------------------------------- ADD BUTTON EVENT LISTENERS ------------------------------- */
+
+const addNewOrderOrderButtonEventListener = () => {
+  document
+    .querySelector("#order_new_print_order_body")
+    .addEventListener("click", () => {
+      /* Collect Input Values and Create New Order Attributes Object */
+      const newOrderAttributesObject = {
+        materialGroup: selectedSelectMaterialMaterialGroupId,
+        process: selectedSelectMaterialProcessId,
+        material: selectedSelectMaterialMaterialId,
+        quality: document.querySelector(
+          "#order_new_prints_quality_option_input_select"
+        ).value,
+        strength: document.querySelector(
+          "#order_new_prints_strength_option_input_select"
+        ).value,
+        color: document.querySelector(
+          "#order_new_prints_color_option_input_select"
+        ).value
+      };
+
+      /* Collect Uploaded File */
+      let formElement = document.querySelector(
+        "#order_new_prints_upload_input_file_form"
+      );
+      let formData = new FormData(formElement);
+
+      /* Assign New Order Attributes to FormData */
+      formData.append("type", "print");
+      formData.append("orderStatus", "Requesting Quote");
+      for (let key in newOrderAttributesObject) {
+        formData.append(key, newOrderAttributesObject[key]);
+      }
+
+      document.querySelector(
+        "#order_new_print_modal"
+      ).innerHTML = loaderElement;
+
+      document.querySelector("#order_new_print_modal").style.height = "30vh";
+
+      /* Submit New Order Attributes */
+      setTimeout(() => {
+        newOrderSubmitSuccess();
+      }, 2000);
+    });
+};
+
+/* Success HTML */
+const newOrderSubmitSuccess = () => {
+  const newOrderSubmitSuccessHTML =
+    "<div id='new_order_submit_success_body'>" +
+    "<div id='new_order_submit_success_text'>" +
+    "Successfully Submitted a New Order" +
+    "</div>" +
+    "</div>" +
+    "<div class='new_order_submit_success_button_body'>" +
+    "<div class='new_order_submit_success_button_text'>" +
+    "Ok" +
+    "</div>" +
+    "</div>" +
+    "<div class='new_order_submit_success_button_body'>" +
+    "<div class='new_order_submit_success_button_text'>" +
+    "Submit New Order" +
+    "</div>" +
+    "</div>";
+
+  document.querySelector(
+    "#order_new_print_modal"
+  ).innerHTML = newOrderSubmitSuccessHTML;
+};
