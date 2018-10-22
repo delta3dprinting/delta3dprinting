181cf39c143feacee4c1594d29462ef4958d2168
Carl Velasco <carlvelasco96@gmail.com>
2018-10-23 10:47:34 +1300

    test

---
+++ components/global/OrderDetails/AwaitingQuote/awaitingQuote.js
@@ -0,0 +1,36 @@
+/* INITIALISATION */
+const awaitingQuoteInit = order => {
+  const orderStatusId = constructOrderStatusId(order.orderStatus);
+  addModal(
+    orderStatusId,
+    orderDetailsModalHeader,
+    awaitingQuoteOrderDetailsModalFooter
+  );
+  constructHTMLStructure(orderStatusId);
+  awaitingQuoteOrderStatusDescriptionBody(order, orderStatusId);
+};
+
+const awaitingQuoteOrderDetailsModalFooter = "<div></div>";
+
+/* Construct Order Status Description Body */
+const awaitingQuoteOrderStatusDescriptionBody = (order, orderStatusId) => {
+  orderStatusDescriptionBodyTabs(orderStatusId, order.delivery);
+  orderStatusDescriptionBodyHeader(order.orderStatus, orderStatusId);
+  awaitingQuoteDescriptionBodyDetails();
+};
+
+const awaitingQuoteDescriptionBodyDetails = () => {
+  const details =
+    "Please wait while we analyse and generate a quote for your order.";
+
+  const orderStatusDescriptionBodyDetailsHTML =
+    "<div id='order_status_description_details_body'>" +
+    "<div id='order_status_description_details_text'>" +
+    details +
+    "</div>" +
+    "</div>";
+
+  document
+    .querySelector("#awaiting_quote_order_status_description_body")
+    .insertAdjacentHTML("beforeend", orderStatusDescriptionBodyDetailsHTML);
+};
--- components/global/OrderDetails/orderDetails.js
+++ components/global/OrderDetails/orderDetails.js
@@ -24,3 +24,145 @@ const viewOrderDetails = orderNumber => {
     }
   });
 };
+
+const constructOrderStatusId = orderStatus => {
+  return orderStatus.toLowerCase().replace(" ", "_");
+};
+
+/* MODAL */
+const orderDetailsModalHeader = "Order Details";
+
+/* HTML STRUCTURE */
+const constructHTMLStructure = orderStatusId => {
+  const htmlStructure =
+    "<div id='" +
+    orderStatusId +
+    "_body' class='order_details_main_body'>" +
+    "<div id='" +
+    orderStatusId +
+    "_order_status_description_body' class='order_status_description_body order_details_body'></div>" +
+    "<div id='" +
+    orderStatusId +
+    "_order_options_details_body' class='order_options_details_body order_details_body'></div>" +
+    "<div id='" +
+    orderStatusId +
+    "_order_details_attached_files_body' class='order_details_attached_files_body order_details_body'></div>" +
+    "<div id='" +
+    orderStatusId +
+    "_order_details_comments_body' class='order_details_comments_body order_details_body'></div>" +
+    "</div>";
+
+  document.querySelector(
+    "#" + orderStatusId + "_modal_body"
+  ).innerHTML = htmlStructure;
+};
+
+// Construct Order Status Description Body Tabs
+const orderStatusDescriptionBodyTabs = (orderStatusId, delivery) => {
+  const orderStatusDescriptionBodyTabsHTML =
+    "<div id='order_status_tabs_body'>" +
+    "<div id='awaiting_quote_order_status_tab_body' class='order_status_tab_body'>" +
+    "<div id='awaiting_quote_order_status_tab_text' class='order_status_tab_text'>" +
+    "Awaiting Quote" +
+    "</div>" +
+    "</div>" +
+    "<div id='awaiting_payment_order_status_tab_body' class='order_status_tab_body'>" +
+    "<div id='awaiting_payment_order_status_tab_text' class='order_status_tab_text'>" +
+    "Awaiting Payment" +
+    "</div>" +
+    "</div>" +
+    "<div id='awaiting_payment_confirmation_order_status_tab_body' class='order_status_tab_body'>" +
+    "<div id='awaiting_payment_confirmation_order_status_tab_text' class='order_status_tab_text'>" +
+    "Awaiting Payment Confirmation" +
+    "</div>" +
+    "</div>" +
+    "<div id='printing_order_order_status_tab_body' class='order_status_tab_body'>" +
+    "<div id='printing_order_order_status_tab_text' class='order_status_tab_text'>" +
+    "Printing Order" +
+    "</div>" +
+    "</div>" +
+    "</div>";
+
+  document
+    .querySelector("#awaiting_quote_order_status_description_body")
+    .insertAdjacentHTML("beforeend", orderStatusDescriptionBodyTabsHTML);
+
+  if (delivery == "Pickup") {
+    const orderStatusPickupTabsHTML =
+      "<div id='ready_for_pickup_order_status_tab_body' class='order_status_tab_body'>" +
+      "<div id='ready_for_pickup_order_status_tab_text' class='order_status_tab_text'>" +
+      "Ready for Pickup" +
+      "</div>" +
+      "</div>" +
+      "<div id='order_completed_order_status_tab_body' class='order_status_tab_body'>" +
+      "<div id='order_completed_order_status_tab_text' class='order_status_tab_text'>" +
+      "Order Completed" +
+      "</div>" +
+      "</div>";
+
+    document
+      .querySelector("#order_status_tabs_body")
+      .insertAdjacentHTML("beforeend", orderStatusPickupTabsHTML);
+  } else {
+    const orderStatusShippingTabsHTML =
+      "<div id='ready_for_shipping_order_status_tab_body' class='order_status_tab_body'>" +
+      "<div id='ready_for_shipping_order_status_tab_text' class='order_status_tab_text'>" +
+      "Ready for Shipping" +
+      "</div>" +
+      "</div>" +
+      "<div id='order_shipped_order_status_tab_body' class='order_status_tab_body'>" +
+      "<div id='order_shipped_order_status_tab_text' class='order_status_tab_text'>" +
+      "Order Shipped" +
+      "</div>" +
+      "</div>" +
+      "<div id='order_completed_order_status_tab_body' class='order_status_tab_body'>" +
+      "<div id='order_completed_order_status_tab_text' class='order_status_tab_text'>" +
+      "Order Completed" +
+      "</div>" +
+      "</div>";
+
+    document
+      .querySelector("#order_status_tabs_body")
+      .insertAdjacentHTML("beforeend", orderStatusShippingTabsHTML);
+  }
+
+  document
+    .querySelector("#" + orderStatusId + "_order_status_tab_body")
+    .classList.add("order_status_tab_body_selected");
+  document
+    .querySelector("#" + orderStatusId + "_order_status_tab_text")
+    .classList.add("order_status_tab_text_selected");
+};
+
+const orderStatusDescriptionBodyHeader = (orderStatus, orderStatusId) => {
+  let heading;
+
+  if (orderStatus == "Awaiting Quote") {
+    heading = "Awaiting Quote";
+  } else if (orderStatus == "Awaiting Payment") {
+    heading = "Waiting for Your Payment";
+  } else if (orderStatus == "Awaiting Payment Confirmation") {
+    heading = "Awaiting Payment Confirmation";
+  } else if (orderStatus == "Printing Order") {
+    heading = "Awaiting Your Order's Completion";
+  } else if (orderStatus == "Ready for Pickup") {
+    heading = "Waiting for Pickup";
+  } else if (orderStatus == "Ready for Shipping") {
+    heading = "Wait while We Ship Your Order";
+  } else if (orderStatus == "Order Shipped") {
+    heading = "Wait for Your Shipment's Arrival";
+  } else if (orderStatus == "Order Completed") {
+    heading = "Your Order has been Completed";
+  }
+
+  const orderStatusDescriptionBodyHeaderHTML =
+    "<div id='order_status_description_heading_body'>" +
+    "<div id='order_status_description_heading_text'>" +
+    heading +
+    "</div>" +
+    "</div>";
+
+  document
+    .querySelector("#" + orderStatusId + "_order_status_description_body")
+    .insertAdjacentHTML("beforeend", orderStatusDescriptionBodyHeaderHTML);
+};
