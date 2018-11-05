103552e1e327db66c1d430720f2d10d5c95625b1
Carl Velasco <carlvelasco96@gmail.com>
2018-11-05 19:45:52 +1300

    test

--- components/global/Tools/dateFormatter.js
+++ components/global/Tools/dateFormatter.js
@@ -139,3 +139,5 @@ const dayDateMonthYearFormat = defaultDate => {
 
   return dayDateMonthYearString;
 };
+
+/* ============================================================================================== */
